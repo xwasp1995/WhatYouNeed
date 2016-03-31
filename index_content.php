012c385b9cdf09244c4b55586489acaf32266fc6
xwasp1995 <xwasp1995@163.com>
2016-03-31 19:45:09 +0800

    Create index_content.php

---
+++ index_content.php
@@ -0,0 +1,135 @@
+
+
+<div style="margin-top:50px"></div>
+<!--成员介绍-->
+<div class="tab-content">
+    <div class="tab-pane active" id="panel-0">
+    <p><img src="./image/menbers.jpeg" width="100%"/></p></div>
+    <div class="tab-pane" id="panel-1">
+    <p><img src="./image/640-8.jpeg" width="100%"/></p></div>
+    <div class="tab-pane" id="panel-2">
+    <p><img src="./image/640-2.jpeg" width="100%"/></p></div>
+    <div class="tab-pane" id="panel-3">
+    <p><img src="./image/640-4.jpeg" width="100%"/></p></div>
+    <div class="tab-pane" id="panel-4">
+    <p><img src="./image/640-3.jpeg" width="100%"/></p></div>
+    <div class="tab-pane" id="panel-5">
+    <p><img src="./image/640-5.jpeg" width="100%"/></p></div>
+    <div class="tab-pane" id="panel-6">
+    <p><img src="./image/640-6.jpeg" width="100%"/></p></div>
+    <div class="tab-pane" id="panel-7">
+    <p><img src="./image/640.jpeg" width="100%"/></p></div>
+    <div class="tab-pane" id="panel-8">
+    <p><img src="./image/640-9.jpeg" width="100%"/></p></div>
+    <div class="tab-pane" id="panel-9">
+    <p><img src="./image/640-7.jpeg" width="100%"/></p></div>
+    <div class="tab-pane" id="panel-10">
+        <!--幻灯片-->
+          <div class="carousel slide" id="carousel-564915">
+            <ol class="carousel-indicators">
+              <li data-slide-to="0" data-target="#carousel-564915" class="active">
+              </li>
+              <li data-slide-to="1" data-target="#carousel-564915">
+              </li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="item active">
+                <img style="width:100%;" alt="我要WhatYouNeed" src="./image/banner1.gif" />
+                <div class="carousel-caption">
+                  <h4>
+                    我要WhatYouNeed
+                  </h4>
+
+                </div>
+              </div>
+              <div class="item">
+                <img style="width:100%;" alt="" src="./image/banner2.gif" />
+                <div class="carousel-caption">
+                  <h4>
+                    我要WhatYouNeed
+                  </h4>
+
+                </div>
+              </div>
+
+            </div> <a class="left carousel-control" data-slide="prev" href="#carousel-564915"></a> <a class="right carousel-control" data-slide="next" href="#carousel-564915"></a>
+          </div>
+
+        <h3>
+        <span class="label label-info">Vivian</span>
+        <span class="label label-info">田心</span>
+        <span class="label label-info">Steven</span>
+        <span class="label label-info">Pisccs</span>
+        <span class="label label-info">Four-X</span></h3><h3>
+        <span class="label label-info">汉森</span>
+        <span class="label label-info">薏仁</span>
+        <span class="label label-info">婧儿</span>
+        <span class="label label-info">Crown</span>
+        <span class="label label-info">高速公鹿</span></h3><h3>
+        <span class="label label-info">炒蟹</span>
+        <span class="label label-info">比金</span>
+        <span class="label label-info">220</span>
+        <span class="label label-info">西米</span>
+        <span class="label label-info">王子越</span></h3><h3>
+        <span class="label label-info">啊哩</span>
+        <span class="label label-info">Carine</span>
+        <span class="label label-info">姗奶奶不会游泳的鱼</span>
+        <span class="label label-info">不会游泳的鱼</span></h3><h3>
+        <span class="label label-info">Gary</span>
+        <span class="label label-info">等等</span>
+        </h3>
+        </div>
+    <div class="tab-pane" id="panel-11">
+    <p><img src="./image/640-10.jpeg" width="100%"/></p></div>
+
+
+
+    <!-- 对话框 -->
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" 
+   aria-labelledby="myModalLabel" aria-hidden="true">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <button type="button" class="close" 
+               data-dismiss="modal" aria-hidden="true">
+                  &times;
+            </button>
+            <h4 class="modal-title" id="myModalLabel">
+              我要投稿
+            </h4>
+         </div>
+         <div class="modal-body">
+ 
+<div class="content">
+</div>
+          <div class="footer">
+          </div>
+         </div><!--body-->
+        </div><!--content-->
+      </div><!--dialog-->
+     </div><!--modal-->
+
+</nav>
+
+<!-- 底部切换栏 -->
+<nav class="navbar navbar-default navbar-fixed-bottom">
+    <div class="tabbable tabs-below" id="tabs"> 
+      <div class="container">
+            <ul class="nav nav-tabs">
+            <li class="active"><a href="#panel-0" data-toggle="tab">全家福</a></li>
+            <li><a href="#panel-1" data-toggle="pill">Blake</a></li>
+            <li><a href="#panel-2" data-toggle="tab">Tom</a></li>
+            <li><a href="#panel-3" data-toggle="tab">Jame</a></li>
+            <li><a href="#panel-4" data-toggle="tab">紫菜姑娘</a></li>
+            <li><a href="#panel-5" data-toggle="tab">丸尾同学</a></li>
+            <li><a href="#panel-6" data-toggle="tab">Frank</a></li>
+            <li><a href="#panel-7" data-toggle="tab">Ninety</a></li>
+            <li><a href="#panel-8" data-toggle="tab">樺璃</a></li>
+            <li><a href="#panel-9" data-toggle="tab">花大钱</a></li>
+            <li><a href="#panel-10" data-toggle="tab">还有还有</a></li>
+            <li><a href="#panel-11" data-toggle="tab">ME</a></li>
+            </ul>
+  </div>
+</div>
+</nav>
+
